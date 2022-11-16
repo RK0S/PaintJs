2ced216e59be8cb134aa24111b43649f24ee3852
Roman <r.l.kosolapov@gmail.com>
2022-11-16 07:33:27 +0300

    add localstorage

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
-const colors = document.getElementsByClassName('jsColor');
+const letColors = document.getElementsByClassName('letColor');
 const range = document.getElementById('jsRange');
 const mode = document.getElementById('jsMode');
 const saveBtn = document.getElementById('jsSave');
 const colorPicker = new iro.ColorPicker('#picker');
+const colorPickerBlock = document.getElementById('picker');
+const constColors = document.getElementsByClassName('constColor');
+const colors = document.getElementsByClassName('controls__color');
 
-const initialColor = '#2c2c2c';
+let colorsList = [];
+if (localStorage.getItem("colors")) {
+    colorsList = JSON.parse(localStorage.getItem("colors"));
+    for (let a of letColors) {
+        a.style.backgroundColor = colorsList[a.id];
+    }
+}
+
+let initialColor = '#2c2c2c';
+
+if (localStorage.getItem('color')) {
+    colorPicker.color.hexString = localStorage.getItem('color');
+    initialColor = colorPicker.color.hexString;
+}
+
 const canvasSize = 700;
 
 canvas.height = canvasSize;
 canvas.width = canvasSize;
+
+let dataURL = localStorage.getItem('paint');
+let img = new Image();
+img.src = dataURL;
+img.onload = function () {
+    ctx.drawImage(img, 0, 0);
+};
 
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, canvasSize, canvasSize);
@@ -47,13 +71,18 @@ function onMouseDown(event){
 
 function onMouseUp(event){
     stopPainting();
+    console.log(1);
+    localStorage.setItem('paint', canvas.toDataURL());
+    console.log(1);
 }
 
-function changeColor(event) {
-    let color = event.target.style.backgroundColor;
+function changeColor() {
+    let color = colorPicker.color.hexString;
+    localStorage.setItem('color', color);
     ctx.strokeStyle = color;
     ctx.fillStyle = color;
-    console.log(event.target.style);
+    replaceColors(color);
+    localStorage.setItem('colors', JSON.stringify(colorsList));
 }
 
 function rangeChange(event) {
@@ -99,7 +128,10 @@ if (canvas) {
     canvas.addEventListener('contextmenu', handleCM);
 }
 
-Array.from(colors).forEach(color => color.addEventListener('click', changeColor));
+// Array.from(colors).forEach(color => color.addEventListener('click', changeColor));
+colorPickerBlock.addEventListener('click', changeColor);
+Array.from(constColors).forEach(color => color.addEventListener('click', getColor));
+Array.from(colors).forEach(color => color.addEventListener('click', getColor));
 
 if (range) {
     range.addEventListener('input', rangeChange);
@@ -116,4 +148,23 @@ if (saveBtn) {
 colorPicker.on('color:change', function(color) {
     ctx.strokeStyle = colorPicker.color.hexString;
     ctx.fillStyle = colorPicker.color.hexString;
+    localStorage.setItem('color', colorPicker.color.hexString);
 });
+
+function replaceColors(color) {
+    if (colorsList.length < 7) {
+        colorsList.unshift(color);
+    } else {
+        colorsList.pop();
+        colorsList.unshift(color);
+    }
+    for (let a of letColors) {
+        a.style.backgroundColor = colorsList[a.id];
+    }
+}
+
+function getColor(e) {
+    const color = e.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
